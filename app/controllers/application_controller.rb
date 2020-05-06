80641a732183dd625a5b0d131e8d0e91efa2ac91
Samantha Jordan <shinyguns@gmail.com>
2020-05-06 21:35:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,6 +28,7 @@ class ApplicationController < Sinatra::Base
   get '/articles/:id' do
     @article = Article.find(params["id"])
     erb :show
+  end
     
   get '/articles/:id/edit' do
     @articles = Article.find(params["id"])
